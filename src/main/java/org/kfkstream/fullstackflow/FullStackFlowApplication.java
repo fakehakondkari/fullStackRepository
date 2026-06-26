9dcb8f78540b3acb678dbb2c0d9c45ea31da6d12
fakeha kondkari <fakehakondkari38@gmail.com>
2026-06-26 20:50:36 +0530

    first commit

---
+++ src/main/java/org/kfkstream/fullstackflow/FullStackFlowApplication.java
@@ -0,0 +1,13 @@
+package org.kfkstream.fullstackflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullStackFlowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/kfkstream/fullstackflow/FullStackFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kfkstream.fullstackflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
